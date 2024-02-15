9f9ccc1fb4c24bea35c620c086748c4e5b366025
Rebeca Miguel <rebecagaspar19@gmail.com>
2024-02-14 21:26:54 -0300

    subindo

--- src/Pages/About/index.jsx
+++ src/Pages/About/index.jsx
@@ -7,6 +7,7 @@ export default function About() {
         <div className={styles.WidthContent}>
           <div className={styles.Header}>Escreva sobre você!!!</div>
           <img className={styles.Img} src="https://static.vecteezy.com/ti/vetor-gratis/p3/2002247-icone-de-avatar-de-mulher-negra-bonita-gratis-vetor.jpg" alt="" />
+          
           <div className={styles.Name}>Rebeca Gaspar Miguel Miguel</div>
           <div className={styles.Cargo}>Desenvolvedora Front-End</div>
           <div className={styles.Desc}>Sou a Rebeca Gaspar Miguel, angolana residente em São Paulo. Sou formada em Administração Pública pela Universidade Federal UNILAB. Tenho formação em Programação Back End pela Toti e estou em fase de transição de carreira. Atualmente estou cursando desenvolvimento Front-End na instituiçao Pretalab.</div>
